7b4619955fc302a765f036576f079bb99e5b7e91
vinod1999 <vinodsasanka1999@gmail.com>
2021-06-21 20:53:15 +0530

    right click mismatch DOT number bug fixed.

--- assets/js/contentScripts/monitor.js
+++ assets/js/contentScripts/monitor.js
@@ -1,52 +1,15 @@
-function getTextFromRightClick() {
+function getTextFromRightClick(event) {
+  if (!(event && event.target)) return;
+
   const DOT_MIN_LENGTH = 5;
   const DOT_MAX_LENGTH = 10;
+  const reDOT = /^[0-9]+$/gim;
 
-  const getText = (event) => {
-    const reDOT = /^[0-9]+$/gim;
+  const target = event.target;
+  if (!(target instanceof HTMLElement)) return;
 
-    const target = event?.target;
-    if (!(target instanceof HTMLElement)) return;
-
-    if (target.nodeName === "INPUT") {
-      const targetText = target?.value?.trim();
-      if (
-        targetText &&
-        targetText.length >= DOT_MIN_LENGTH &&
-        targetText.length <= DOT_MAX_LENGTH &&
-        reDOT.test(targetText)
-      ) {
-        return targetText;
-      }
-    }
-
-    if (!target.hasChildNodes()) return;
-
-    const originalTargetElement = target.cloneNode(true);
-
-    const textNodesArray = Array.from(target.childNodes).filter(
-      (node) => node.nodeName === "#text"
-    );
-    if (!textNodesArray.length) return;
-
-    textNodesArray.forEach((textNode) => {
-      let textArray = textNode?.nodeValue?.split(/(\s|\n|\r)/gim);
-      if (!textArray.length) return;
-
-      const fragment = new DocumentFragment();
-      textArray.forEach((text) => {
-        if (/\s/gim.test(text)) return;
-
-        const span = document.createElement("span");
-        span.innerText = text;
-        fragment.append(span, " ");
-      });
-      textNode.replaceWith(fragment);
-    });
-
-    const targetSpan = document.elementFromPoint(event.clientX, event.clientY);
-    target.replaceWith(originalTargetElement);
-    const targetText = targetSpan?.innerText?.trim();
+  if (target.nodeName === "INPUT" && target.value) {
+    const targetText = target.value?.trim();
     if (
       targetText &&
       targetText.length >= DOT_MIN_LENGTH &&
@@ -55,28 +18,64 @@ function getTextFromRightClick() {
     ) {
       return targetText;
     }
-    return false;
+  }
+
+  if (!target.hasChildNodes()) return;
+
+  const originalTargetElement = target.cloneNode(true);
+
+  const textNodesArray = Array.from(target.childNodes).filter(
+    (node) => node.nodeName === "#text"
+  );
+  if (!textNodesArray.length) return;
+
+  textNodesArray.forEach((textNode) => {
+    let textArray = textNode?.nodeValue?.split(/(\s|\n|\r)/gim);
+    if (!textArray.length) return;
+
+    const fragment = new DocumentFragment();
+    textArray.forEach((text) => {
+      if (/\s/gim.test(text)) return;
+
+      const span = document.createElement("span");
+      span.innerText = text;
+      fragment.append(span, " ");
+    });
+    textNode.replaceWith(fragment);
+  });
+
+  const targetSpan = document.elementFromPoint(event.clientX, event.clientY);
+  target.replaceWith(originalTargetElement);
+
+  const targetText = targetSpan?.innerText?.trim();
+  if (
+    targetText &&
+    targetText.length >= DOT_MIN_LENGTH &&
+    targetText.length <= DOT_MAX_LENGTH &&
+    reDOT.test(targetText)
+  ) {
+    return targetText;
+  }
+  return false;
+}
+
+function main() {
+  if (!(chrome.runtime && chrome.runtime.id)) return;
+
+  let newEvent;
+  const handleContextMenu = (event) => (newEvent = event);
+
+  const handleMessage = (request, _, sendResponse) => {
+    if (request.message === "DOTScannerClicked") {
+      const rightClickText = getTextFromRightClick(newEvent);
+      sendResponse({ rightClickText });
+    }
   };
 
-  const handleContextMenu = (event) => {
-    if(!(chrome.runtime && chrome.runtime.id)) return;
-    
-    const handleMessage = (request, _, sendResponse) => {
-      if (request.message === "DOTScannerClicked") {
-        const rightClickText = getText(event);
-      
-        if(rightClickText) {
-          sendResponse({ rightClickText })
-        }
-      }
-      chrome.runtime.onMessage.removeListener(handleMessage);
-      
-    };
-    chrome.runtime.onMessage.addListener(handleMessage);
-  };
   document.addEventListener("contextmenu", handleContextMenu);
+  chrome.runtime.onMessage.addListener(handleMessage);
 }
 
 (() => {
-  getTextFromRightClick();
+  main();
 })();
